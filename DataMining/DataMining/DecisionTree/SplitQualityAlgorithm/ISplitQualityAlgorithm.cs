34becc522f4330f2e60a51c8fa237eb93cbc250a
beller0ph0nt <kras_den@rambler.ru>
2015-03-19 23:38:06 +0700

    Убраны мтарые куски кода из интерфейса ISplitQualityAlgorithm

--- DataMining/DataMining/DecisionTree/SplitQualityAlgorithm/ISplitQualityAlgorithm.cs
+++ DataMining/DataMining/DecisionTree/SplitQualityAlgorithm/ISplitQualityAlgorithm.cs
@@ -7,10 +7,7 @@ namespace DataMining.DecisionTree.SplitQualityAlgorithm
 {
     public interface ISplitQualityAlgorithm
     {
-        //double Quality { get; set; }
-
         double CalcSplitQuality(List<List<double>> splits, int generalSplitCount);
         int Compare(double firstQuality, double secondQuality);
-        //int CompareTo(object obj);
     }
 }
