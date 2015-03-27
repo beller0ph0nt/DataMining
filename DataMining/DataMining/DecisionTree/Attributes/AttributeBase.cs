bcdc842e7926af5bac9cb1bd190a9b2340dca353
beller0ph0nt <kras_den@rambler.ru>
2015-03-27 23:57:43 +0700

    Добавлены комментарии

--- DataMining/DataMining/DecisionTree/Attributes/AttributeBase.cs
+++ DataMining/DataMining/DecisionTree/Attributes/AttributeBase.cs
@@ -6,25 +6,54 @@ using DataMining.DecisionTree.Splits;
 
 namespace DataMining.DecisionTree.Attributes
 {
+    /// <summary>
+    /// Типы аттрибутов
+    /// </summary>
     public enum AttributType
     {
-        Numerical,
-        Categorical
+        Numerical,      // Числовой
+        Categorical     // Категориальный
     }
 
+    /// <summary>
+    /// Базовый класс аттрибутов
+    /// </summary>
     public abstract class AttributeBase
     {
+        #region Свойства
+
+        /// <summary>
+        /// Идентификатор аттрибута
+        /// </summary>
         public int Id { get; private set; }
+
+        /// <summary>
+        /// Тип аттрибута
+        /// </summary>
         public AttributType Type { get; private set; }
+
+        /// <summary>
+        /// Список значений аттрибута
+        /// </summary>
         public List<object> Values { get; private set; }
 
-        public AttributeBase(int id, AttributType type, List<object> values)
+        #endregion
+
+        protected AttributeBase(int id, AttributType type, List<object> values)
         {
             Id = id;
             Type = type;
             Values = values;
         }
 
+        #region Методы
+
+        /// <summary>
+        /// Метод, разделяющий аттрибут на подмножества
+        /// </summary>
+        /// <returns>Список разделений</returns>
         public abstract List<AttributeBase> Split();
+
+        #endregion
     }
 }
