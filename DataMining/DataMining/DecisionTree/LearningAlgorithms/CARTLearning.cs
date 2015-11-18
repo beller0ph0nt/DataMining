779f892319580f5b6c512fd055dd8f7dbd19e45f
Denis <kras_den@rambler.ru>
2015-11-18 23:38:01 +0700

    заложены функции для алгоритма построения структуры дерева решений

--- DataMining/DataMining/DecisionTree/LearningAlgorithms/CARTLearning.cs
+++ DataMining/DataMining/DecisionTree/LearningAlgorithms/CARTLearning.cs
@@ -9,36 +9,25 @@ namespace DataMining.DecisionTree.LearningAlgorithm
 {
     public class CARTLearning : ILearningAlgorithm
     {
-		//private CARTTree<Split> _tree;	// обучаемое дерево
         private ISplitQualityAlgorithm _splitQuality;
 
-		public CARTLearning(CARTTree<Split> tree)
+		public CARTLearning()
         {
-            //_tree = tree;
             _splitQuality = new GiniSplit();
         }
 
 		public void Training(DataTable table)
 		{
-			// после обучения должен выплюнуть готовое дерево CARTTree<Split> _tree
+			// !!! после обучения должен выплюнуть готовое дерево CARTTree<Split> _tree !!!
 
-			//int id = _tree.CreateRoot();	// Создать корень у дерева
-			//Split s = new Split(table);		// Получить лучшее разбиение
+			ICARTNode<Split> root = CARTNodeFactory<Split>.GetRoot ();
+			CreateTree (root, table);
+		}
 
-			// Создать у текущего узла два листа
-			// В левый лист поместить [0] разбиение
-			// В правый лист поместить [1] разбиение
-
-			// Цикл по всем листам
-			// Проверяем родителя листа. Выходим из цикла, когда у всех родителей
-
-
-			//-----------------------------------------
-			// Вначале должна быть инициаллизация дерева. Должен появиться корень и 2 листа
-			// Далее начинается цикл по всем листам в дереве.
-			// Вычисляем лучшее разбиение для текущего листа.
-			// Если в разбиении получилось только одно множество, то такое разбиение пропускаем.
-			// Если в разбиении 2 подмножества, то родительский лист надо заменить на узел и к нему привязать 2-а листа и заполнить их разбиениями.
+		private void CreateTree(ICARTNode<Split> node, DataTable table)
+		{
+			Split s = new Split (table, _splitQuality);
+			node.Variable = s;
 		}
     }
 }
